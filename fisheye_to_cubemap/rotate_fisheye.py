0c8875e7a7494234fd4ae875267b48c8f61a20e5
nobu74658 <hiroki-pon@outlook.jp>
2024-05-30 11:31:36 +0900

    folderを選択して、回転する関数を作成

--- fisheye_to_cubemap/rotate_fisheye.py
+++ fisheye_to_cubemap/rotate_fisheye.py
@@ -33,20 +33,20 @@ def fisheye_to_cubemap_folder(input_folder, output_folder):
     for image_path in os.listdir(input_folder):
         rotate_fisheye(input_folder + image_path, output_folder + image_path)
 
-inputFile = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/test.png'
-firstRotateFile = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/first_rotate.png'
-firstRotate90File = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/first_rotate90.png'
-tmpFile = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/tmp.png'
-outputFile = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/output.png'
-outputRotate90File = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/output_rotate90.png'
-rotate = mp.pi/4
-rotate_fisheye(inputFile, firstRotateFile, rotate)
-## 画像を90度回転
-img = cv2.imread(firstRotateFile)
-img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-cv2.imwrite(firstRotate90File, img)
-rotate_fisheye(firstRotate90File, outputFile, rotate)
-## 画像を-90度回転
-img = cv2.imread(outputFile)
-img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-cv2.imwrite(outputRotate90File, img)
+
+input_folder = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/input/'
+output_folder = '/Users/nobu/research/mmdetection-grape/fisheye_to_cubemap/output/'
+def rotate_fisheye_folder(input_folder, output_folder, rotate):
+    for image_path in os.listdir(input_folder):
+        rotate_fisheye(input_folder + image_path, output_folder + image_path, rotate)
+        # 画像を90度回転
+        img = cv2.imread(output_folder + image_path)
+        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        cv2.imwrite(output_folder + image_path, img)
+        rotate_fisheye(output_folder + image_path, output_folder + image_path, rotate)
+        # 画像を-90度回転
+        img = cv2.imread(output_folder + image_path)
+        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+        cv2.imwrite(output_folder + image_path, img)
+
+rotate_fisheye_folder(input_folder, output_folder, mp.pi/6)
